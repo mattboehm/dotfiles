6651175a63d014592050852447c3cf729a2d3044
Matthew Boehm <boehm.matthew@gmail.com>
2014-03-18 22:43:33 -0400

    Add battery widget to awesomewm

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -149,6 +149,67 @@ mytasklist.buttons = awful.util.table.join(
                                               if client.focus then client.focus:raise() end
                                           end))
 
+--battery info widget
+battery_widget = widget({ type = "textbox" })
+function batteryInfo(adapter)
+   local fcur = io.open("/sys/class/power_supply/"..adapter.."/charge_now")    
+   local fcap = io.open("/sys/class/power_supply/"..adapter.."/charge_full")
+   local fsta = io.open("/sys/class/power_supply/"..adapter.."/status")
+   if fcur and fcap and fsta then
+        local cur = fcur:read()
+        local cap = fcap:read()
+        local sta = fsta:read()
+        local battery = math.floor(cur * 100 / cap)
+        if sta:match("Charging") then
+            dir = "^"
+        elseif sta:match("Discharging") then
+            dir = "v"
+        else
+            dir = ""
+        end
+       fcur:close()
+       fcap:close()
+       fsta:close()
+       return {battery,dir}
+   end
+end 
+
+function batteryCheck(adapter)
+    local batInfos = batteryInfo(adapter)
+    if batInfos then
+        local battery = batInfos[1]
+        local dir = batInfos[2]
+        if dir:match("v") and tonumber(battery) < 10 then
+               naughty.notify({ preset = naughty.config.presets.critical,
+                                title = "Battery Low",
+                                text = " ".. battery .. "% left",
+                                timeout = 2,
+                                font = "Liberation 11", })
+       end
+       infos = " " .. dir .. battery .. "% "
+   else
+       infos = "A/C"
+   end
+   battery_widget.text = infos
+end
+
+function batteryShow(adapter)
+    local batInfos =  batteryInfo(adapter)
+    if batInfos then
+        local battery = batInfos[1]
+        local dir = batInfos[2]
+        infos = " " .. dir .. battery .. "% " 
+    else
+       infos = "absente"
+    end
+      naughty.notify({title = "Battery",text = infos})
+
+end
+
+battery_timer = timer({timeout = 2})
+battery_timer:add_signal("timeout", function()  batteryCheck("BAT1") end)
+battery_timer:start()
+
 for s = 1, screen.count() do
     -- Create a promptbox for each screen
     mypromptbox[s] = awful.widget.prompt({ layout = awful.widget.layout.horizontal.leftright })
@@ -180,6 +241,7 @@ for s = 1, screen.count() do
         },
         mylayoutbox[s],
         mytextclock,
+	battery_widget,
         s == 1 and mysystray or nil,
         mytasklist[s],
         layout = awful.widget.layout.horizontal.rightleft
